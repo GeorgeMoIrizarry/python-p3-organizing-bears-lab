07cf613517ed2fe1de44a1ce4456a770acb4ac10
George Manuell Irizarry <mojomojoke9@gmail.com>
2024-05-24 02:56:02 -0400

    bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ('Mr. Chocolate', 10, 'M', 'Brown', 'Lazy');
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ('Rowdy', 3, 'M', 'Black', 'Rowdy');
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ('Tabitha', 7, 'F', 'Light Brown', 'Eager');
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ('Sergant Brown', 3, 'M', 'Brown', 'Rowdy');
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ('Melissa', 13, 'F', 'Black and White', 'Relaxed');
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ('Grinch', 9, 'M', 'White', 'Grouchy');
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ('Wendy', 2, 'F', 'Red', 'Energetic');
+INSERT INTO bears (sex) VALUES ('M');
